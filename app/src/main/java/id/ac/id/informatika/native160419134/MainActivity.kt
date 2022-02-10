fefcc251e9ac8b6c0feee23677c636a36dab7838
kiky <kikyfadhilah2001@gmail.com>
2022-02-10 11:18:42 +0700

    variabel login sudah di tambahkan

--- app/src/main/java/id/ac/id/informatika/native160419134/MainActivity.kt
+++ app/src/main/java/id/ac/id/informatika/native160419134/MainActivity.kt
@@ -3,6 +3,10 @@ package id.ac.id.informatika.native160419134
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//perlu bikin fitur login
+var login:Boolean = false
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
